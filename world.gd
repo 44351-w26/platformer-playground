f559150a0d8da1ec2e8184c3f489f623f0bdc8a6
Nathan Eloe <powerofazure@gmail.com>
2026-02-27 12:40:43 -0600

    SECRETS

--- world.gd
+++ world.gd
@@ -17,3 +17,16 @@ func _on_kill_plane_body_entered(body: Node2D) -> void:
 
 func _on_enemy_hit_player() -> void:
 	$Player.position = $PlayerSpawn.position
+
+
+func _on_event_trigger_body_entered(body: Node2D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Secret, "self_modulate",
+						 Color.TRANSPARENT,.5)
+	
+
+
+func _on_event_trigger_body_exited(body: Node2D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Secret, "self_modulate",
+						 Color.WHITE,.5)
